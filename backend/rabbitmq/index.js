e5a686c94e919f077f99d9ca9aa60f7710f540c8
dgorpinic <dennis.gorpinic@fh-bielefeld.de>
2022-05-22 17:49:17 +0200

    added rabbitmq

---
+++ backend/rabbitmq/index.js
@@ -0,0 +1,35 @@
+let amqp = require('amqplib');
+
+require('dotenv').config()
+
+const connection = amqp.connect(process.env.RABBITMQ_URL, (error0, connection) => {
+  if (error0) throw error0;
+  return connection;
+});
+
+function getConnection() {
+  return connection
+}
+
+async function listen(queueName, routingKey, callback){
+
+    try {
+
+        const connection = await getConnection()
+        const channel = await connection.createChannel()
+
+        await channel.assertExchange(process.env.EXCHANGE, "topic", {durable: false})
+
+        const queue = await channel.assertQueue("Bank", {durable: true, exclusive: true})
+        await channel.bindQueue(queue.queue, process.env.EXCHANGE, routingKey)
+
+        await channel.consume(queue.queue, (message) => {
+            callback(message.content.toString())
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+module.exports = {getConnection, receive}
