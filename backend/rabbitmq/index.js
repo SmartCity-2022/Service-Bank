6070e487cb25f5b16f825f1f493734039188b5eb
Dennis <55973868+phoenixdg@users.noreply.github.com>
2022-05-23 13:52:07 +0200

    Update index.js

--- backend/rabbitmq/index.js
+++ backend/rabbitmq/index.js
@@ -18,7 +18,7 @@ async function listen(queueName, routingKey, callback){
         const connection = await getConnection()
         const channel = await connection.createChannel()
 
-        await channel.assertExchange(process.env.EXCHANGE, "topic", {durable: false})
+        await channel.assertExchange(process.env.EXCHANGE, "topic", {durable: true})
 
         const queue = await channel.assertQueue("Bank", {durable: true, exclusive: true})
         await channel.bindQueue(queue.queue, process.env.EXCHANGE, routingKey)
