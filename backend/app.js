4dab10aeee8ea18adf45a03dcc0098dd32f07073
dgorpinic <dennis.gorpinic@fh-bielefeld.de>
2022-06-12 19:49:46 +0200

    add auth dependencies to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,30 @@
 const express = require('express')
-const app = express()
 var cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 require('dotenv').config()
 
 const models = require('./models')()
+const rmq = require("./rabbitmq")
 
-app.use(cors())
-app.use(express.json())
+const app = express()
+
 app.set('sequelize', models)
+
+app.use(express.json())
+app.use(cors({origin: true, credentials: true}))
+app.use(cookieParser())
 app.use(require('./routes'))
 
-app.listen(process.env.API_PORT, () => {
-  console.log('Backend listening on:' + process.env.API_URL + process.env.API_PORT)
+app.get('sequelize').sync().then(async () => {
+
+  app.listen(process.env.API_PORT, async () => {
+    console.log('Backend listening on: ' + process.env.API_URL + process.env.API_PORT)
+    
+    await rmq.listen("", "service.world", (secret) => {
+      process.env.SECRET = secret
+    })
+    await rmq.publish("service.hello", "bankenportal")
+  
+  })
 })
