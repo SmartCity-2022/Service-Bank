154fbb88490f33e025ecbd528e245412e182f6e9
dgorpinic <dennis.gorpinic@fh-bielefeld.de>
2022-06-15 04:45:37 +0200

    add Components

--- frontend/smartcity_bank/src/components/AccountCard.js
+++ frontend/smartcity_bank/src/components/AccountCard.js
@@ -6,25 +6,36 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-function card(name, balance) {
-    return (
-    <React.Fragment>
-        <CardContent>
-        <Typography align="center" variant="h5" component="div">
-            {name}
-        </Typography>
-        <Typography align="center" variant="body2">
-            {balance}
-        </Typography>
-        </CardContent>
-    </React.Fragment>
-    )
-}
+import axios from "axios"
 
-export default function OutlinedCard(name, balance) {
+export default function AccountCard(props) {
+  
+  const handleDelete = async () => {
+
+    const url = process.env.REACT_APP_API_URL + "/account/" + props.id
+
+    await axios.delete(url, {withCredentials: true})
+      .then(() => {
+        console.log("DELETED")
+    })
+  }
+  
   return (
     <Box sx={{ minWidth: 275 }}>
-     {card(name, balance)}
+      <Card variant="outlined">
+        <CardContent>
+        <Typography variant="h5" component="div">
+            {props.name}
+        </Typography>
+        <Typography variant="body2">
+            {"Kontostand: " + props.balance}
+        </Typography>
+        </CardContent>
+        <CardActions>
+        <Button size="small" href={props.accountLink}>DETAILS</Button>
+        <Button variant="outlined" color="error" onClick={handleDelete}>LÖSCHEN</Button>
+        </CardActions>  
+      </Card>
     </Box>
   );
 }
